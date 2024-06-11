73ec0121fda08a0b7b7fcdc4ca0e6a8a30fd29cb
CharlotteLekoro <156782118+CharlotteLekoro@users.noreply.github.com>
2024-06-11 15:09:09 +0200

    properties object added and populated

--- index.ts
+++ index.ts
@@ -33,7 +33,7 @@ function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
     const iconDisplay = isLoyalty ? '⭐' : ''
     reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay
 }
-showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
+
 
 const you: {
     firstName: string;
@@ -51,6 +51,65 @@ const you: {
     stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
 }
 
+//Properties
+type Property = {
+    image: string;
+    title: string;
+    price: number;
+    location: {
+      firstLine: string;
+      city: string;
+      code: number;
+      country: string;
+    };
+    contact: string;
+    isAvailable: boolean;
+  };
+  
+  const properties: Property[] = [
+    {
+      image: '',
+      title: 'Colombian Shack',
+      price: 45,
+      location: {
+        firstLine: 'shack 37',
+        city: 'Bogota',
+        code: 45632,
+        country: 'Colombia'
+      },
+      contact: 'marywinkle@gmail.com',
+      isAvailable: true
+    },
+    {
+      image: '',
+      title: 'Polish Cottage',
+      price: 34,
+      location: {
+        firstLine: 'no 23',
+        city: 'Gdansk',
+        code: 343903,
+        country: 'Poland'
+      },
+      contact: 'garydavis@hotmail.com',
+      isAvailable: false
+    },
+    {
+      image: '',
+      title: 'London Flat',
+      price: 23,
+      location: {
+        firstLine: 'flat 15',
+        city: 'London',
+        code: 35433,
+        country: 'United Kingdom'
+      },
+      contact: 'andyluger@aol.com',
+      isAvailable: true
+    }
+  ];
+  
+
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
 console.log(you.userName)
 
 
@@ -62,4 +121,5 @@ function populateUser(isReturning : boolean, userName : string ) {
 }
 
 populateUser(you.isReturning, you.userName)
+
 
