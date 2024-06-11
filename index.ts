077c0e94f8e7affcb47ff38c087b0737e117ec67
CharlotteLekoro <156782118+CharlotteLekoro@users.noreply.github.com>
2024-06-11 14:48:10 +0200

    utils file created

--- index.ts
+++ index.ts
@@ -1,15 +1,65 @@
-const reviewTotalDisplay = document.querySelector('#reviews')
+// Object Types Challenge
+// Based on what we discussed we need to make up our Property Objects and array,
+// can you create that array, making sure to assign the correct Types?
+
 const returningUserDisplay = document.querySelector('#returning-user')
 const userNameDisplay = document.querySelector('#user')
+const reviewTotalDisplay = document.querySelector('#reviews')
+import { showReviewTotal, populateUser } from './utils'
+let isOpen : boolean
 
-export function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
+const reviews : {
+    
+        name: 'Sheia',
+        stars: 5,
+        loyaltyUser: true,
+        date: '01-04-2021'
+    },
+    {
+        name: 'Andrzej',
+        stars: 3,
+        loyaltyUser: false,
+        date: '28-03-2021'
+    },
+    {
+        name: 'Omar',
+        stars: 4,
+        loyaltyUser: true,
+        date: '27-03-2021'
+    },
+
+
+function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
     const iconDisplay = isLoyalty ? '⭐' : ''
     reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay
 }
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
 
-export function populateUser(isReturning : boolean, userName: string ) {
+const you: {
+    firstName: string;
+    lastName: string;
+    isReturning: boolean;
+    age: number;
+    stayedAt: string[]
+}
+
+const you: {
+    firstName: 'Bobby',
+    lastName: 'Brown',
+    isReturning: true,
+    age: 35,
+    stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
+}
+
+console.log(you.userName)
+
+
+function populateUser(isReturning : boolean, userName : string ) {
     if (isReturning == true){
         returningUserDisplay.innerHTML = 'back'
     }
     userNameDisplay.innerHTML = userName
 }
+
+populateUser(you.isReturning, you.userName)
+
